20677a4c1a5a9c23511f89b20a48c8c39e620eeb
Rafael Lopes <rafa_lopes_leme@hotmail.com>
2021-11-07 16:56:33 -0300

    Added interval cleanup function for Timer component

--- Modulo2/react-hello/src/components/Timer.js
+++ Modulo2/react-hello/src/components/Timer.js
@@ -5,9 +5,16 @@ export default function Timer() {
 
   useEffect(() => {
     // Forma de incrementar o estado atual de uma variável
-    setInterval(() => {
+    const interval = setInterval(() => {
       setValue((currentValue) => currentValue + 1);
     }, 1000);
+
+    // Função de limpeza, prevenindo memory leak já que o interval continuará caso esta function não seja retornada
+    // O erro causado sem ela é o 'cant perform a React state update on an unmounted component'
+    return () => {
+      console.log("Limpando interval");
+      clearInterval(interval);
+    };
   }, []);
 
   return <span className="bg-red-200 p-3 font-semibold">{value}</span>;
