c06f573db952626baa225a87feb40d0aeb3e5fe9
Rafael Lopes <rafa_lopes_leme@hotmail.com>
2021-11-07 15:09:54 -0300

    Added useState hook and onChange event

--- Modulo2/react-hello/src/App.js
+++ Modulo2/react-hello/src/App.js
@@ -1,17 +1,39 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
-  console.log("Teste no console do navegador");
+  // Hook para estado
+  const [name, setName] = useState("Rafael");
+
+  // Closure para alterar o estado
+  function handleNameChange(event) {
+    const newName = event.currentTarget.value;
+    setName(newName);
+  }
 
   return (
     // Fragment, pode ser utilizado como uma div quando não há necessidade de estilização por exemplo.
     <>
       <Header size="large">react-hello</Header>
       <Main>
-        <div className="container mx-auto p-4">
-          <p>O seu nome é Rafael, com 6 caracteres, e você possui 20 anos.</p>
+        <div className="flex flex-col my-4">
+          <label className="text-sm mb-1" htmlFor="inputName">
+            Digite o seu nome:
+          </label>
+          <input
+            autoFocus
+            id="inputName"
+            className="border p-1"
+            type="text"
+            value={name}
+            onChange={handleNameChange}
+          />
         </div>
+        <p>
+          O seu nome é {name}, com {name.length} caracteres, e você possui 20
+          anos.
+        </p>
       </Main>
     </>
   );
