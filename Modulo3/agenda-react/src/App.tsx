1af5427f64910c29edd99e3dcd626b343599a018
Rafael Lopes <rafa_lopes_leme@hotmail.com>
2021-11-20 16:16:34 -0300

    Added agenda-react-backend code, and did the first integration with it

--- Modulo3/agenda-react/src/App.tsx
+++ Modulo3/agenda-react/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { getEventsEndpoint } from "./backend";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getEventsEndpoint().then((events) => {
+    for (const event of events) {
+      console.log(event.desc);
+    }
+  });
+
+  return <div>Olá</div>;
 }
 
 export default App;
--- Modulo3/readme.md
+++ Modulo3/readme.md
@@ -1,3 +1,10 @@
 # Criando a app utilizando a template de Typescript
 
 npx create-react-app agenda-react --template typescript
+
+# Backend da app: agenda-react-backend
+
+1. Instalar as dependências com npm install, e então rodar a aplicação com npm start.
+   Para subir o servidor sem auth, adicione o parametro noauth depois do start.
+
+2. Um vez em pé, ela servirá os endpoints para a app agenda-react.
